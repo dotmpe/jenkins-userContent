3ecc76af5fba5d56bfd02eb52af5e3b966197f1e
B. van Berkum <dev@dotmpe.com>
2016-03-19 20:32:41 +0100

    JS on Job listing table, CSS on headers/task list.
    - Removing emtpy build counts for folder rows with extras JS.
    - Style for headers h1/h2, and task list items.

--- extras.js
+++ extras.js
@@ -1,1 +1,15 @@
-window.jQuery('table#projectstatus th[width=1]').attr('width', '');
+var jQ = window.jQuery; // window.$ is something else
+
+/* Reset status icon column to autowidth */
+jQ('table#projectstatus th[width=1]').attr('width', '');
+
+/* Go for table folder rows, remove the build counts labels/icons if none are available */
+jjQ(document).ready(function() {
+  jQ('td img.icon-folder').parent().parent().each(function(x,y,z) {
+    jQ('.status_success,.status_unstable,.status_failed', y).each(function(x,statusSpan,z) {
+      if (jQ(statusSpan).text().length === 0) {
+        jQuery(statusSpan).remove();
+      }
+    });
+  })
+});
