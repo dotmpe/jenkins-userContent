e7a070f91bc0161af015a5252a5ea164f6d15b17
B. van Berkum <dev@dotmpe.com>
2016-03-19 20:38:47 +0100

    JS syntax style fixes

--- extras.js
+++ extras.js
@@ -4,12 +4,12 @@ var jQ = window.jQuery; // window.$ is something else
 jQ('table#projectstatus th[width=1]').attr('width', '');
 
 /* Go for table folder rows, remove the build counts labels/icons if none are available */
-jjQ(document).ready(function() {
-  jQ('td img.icon-folder').parent().parent().each(function(x,y,z) {
-    jQ('.status_success,.status_unstable,.status_failed', y).each(function(x,statusSpan,z) {
+jQ(window.document).ready(function() {
+  jQ('td img.icon-folder').parent().parent().each(function(x,y) {
+    jQ('.status_success,.status_unstable,.status_failed', y).each(function(x2,statusSpan) {
       if (jQ(statusSpan).text().length === 0) {
-        jQuery(statusSpan).remove();
+        jQ(statusSpan).remove();
       }
     });
-  })
+  });
 });
